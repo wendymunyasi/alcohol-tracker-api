01f206d74131b60b43dd305a0b91ccbfad3ed440
wendymunyasi <wendymunyasi@gmail.com>
2020-07-13 20:20:59 +0300

    Fix bugs.

--- mysite/settings.py
+++ mysite/settings.py
@@ -145,13 +145,15 @@ CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:4200",
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 # Email configurations remember to install python-decouple
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_PORT = config('EMAIL_PORT')
+
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
