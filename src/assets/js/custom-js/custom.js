9a60397ab894a5c4aca7028273bd4e98bab90360
wendymunyasi <wendymunyasi@gmail.com>
2020-07-16 12:47:15 +0300

    Add animations to the landing page.

--- src/assets/js/custom-js/custom.js
+++ src/assets/js/custom-js/custom.js
@@ -2,9 +2,20 @@ $(function() {
   var effects = 'animated bounce';
   var effectsEnd = 'animationend onAnimationEnd mozAnimationEnd webkitAnimationEnd';
 
-  $('a.button').hover(function() {
+  $('button.notify').hover(function() {
     $(this).addClass(effects).one(effectsEnd, function() {
       $(this).removeClass(effects);
     });
   });
 });
+
+
+
+$(document).ready(function() {
+  $('.popup').click(function() {
+    var src = $(this).attr('src'); 
+    $('.modal').modal('show');
+    $('#popup-img').attr('src', src)  
+  });
+});
+
